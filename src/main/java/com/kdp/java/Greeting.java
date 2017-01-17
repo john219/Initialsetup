af56b03b441a66cf8522ac26adf87c81ec457404
John <johnmichael219sar@gmail.com>
2017-01-17 02:34:27 -0500

    src_check_in

---
+++ src/main/java/com/kdp/java/Greeting.java
@@ -0,0 +1,10 @@
+package com.kdp.java;
+
+public class Greeting
+{
+     public String greet(String name) 
+	{
+		return "Hello "+ name + "!!!";
+	}
+}
+ 
---
+++ src/main/java/com/kdp/java/Helloworld.java
@@ -0,0 +1,12 @@
+package com.kdp.java;
+
+public class Helloworld 
+{
+   public static void main(String[] args) 
+{
+      // Prints "Hello, World" in the terminal window.
+	Greeting obj = new Greeting();
+      System.out.println(obj.greet("Jenkins"));
+      //System.out.println("Hello, World");
+   }
+}
